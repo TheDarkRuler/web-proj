866780aaaf7476738e16bb70ededcc6a982c8383
NickGhignatti <nick.ghignatti@gmail.com>
2023-11-26 16:18:17 +0100

    Add stuff

--- frontend/script/logfade.js
+++ frontend/script/logfade.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var popupForm = document.getElementById('container');
-    var scrollPositionToShowForm = 500; // Adjust this value as needed
+    var scrollPositionToShowForm = 10; // Adjust this value as needed
 
     window.addEventListener("scroll", () => {
         console.log(document.documentElement.scrollTop);
