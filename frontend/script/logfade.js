237c8f2ac75c13da8a9d200fbf9be753f1538663
NickGhignatti <nick.ghignatti@gmail.com>
2023-11-26 18:00:23 +0100

    Working on mobile login signup switch

--- frontend/script/logfade.js
+++ frontend/script/logfade.js
@@ -1,15 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     var popupForm = document.getElementById('container');
     var scrollPositionToShowForm = 10; // Adjust this value as needed
+    let switched = false;
 
     window.addEventListener("scroll", () => {
-        console.log(document.documentElement.scrollTop);
-        document.documentElement.scrollTop > scrollPositionToShowForm ?
+        let scroll = document.documentElement.scrollTop;
+        console.log(scroll);
+        scroll > scrollPositionToShowForm ?
             popupForm.style.animation = 'fadeIn 1s forwards' : popupForm.style.animation = 'fadeOut 1s forwards';
+
     });
+
     const signUpButton = document.getElementById('signUp');
     const signInButton = document.getElementById('signIn');
     const container = document.getElementById('container');
+    const linkToSwitchUp = document.getElementById('switchToSignUp');
+    const singUpPanel = document.querySelector('.sign-up-container');
+    const signInPanel = document.querySelector('.sign-in-container');
+
+    linkToSwitchUp.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+        if (window.innerWidth <= 600) {
+            singUpPanel.style.visibility = 'visible';
+            signInPanel.style.visibility = 'hidden';
+        }
+    });
 
     signUpButton.addEventListener('click', () => {
         container.classList.add("right-panel-active");
