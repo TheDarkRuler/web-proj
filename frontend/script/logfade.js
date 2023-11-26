4bdddbabb1f0d6f56c5df305a59ea962d7707f48
NickGhignatti <nick.ghignatti@gmail.com>
2023-11-26 16:32:56 +0100

    Responsive sign in on going

--- frontend/script/logfade.js
+++ frontend/script/logfade.js
@@ -7,16 +7,15 @@ document.addEventListener('DOMContentLoaded', function () {
         document.documentElement.scrollTop > scrollPositionToShowForm ?
             popupForm.style.animation = 'fadeIn 1s forwards' : popupForm.style.animation = 'fadeOut 1s forwards';
     });
-});
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
 
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+    signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+    });
 
-signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
+    signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+    });
 });
