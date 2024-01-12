b65691496ae68015d36c01f3dc8caf318551c115
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2024-01-12 16:54:47 +0100

    added header behaviour

---
+++ frontend/script/hp-header-up.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var header = document.getElementById('header');
+    var scrollDown = 0;
+    var scrollDirection = false; // true == down | false == top
+    var scrollPositionToShow = 10;
+
+    window.addEventListener('scroll', function () {
+        if (scrollDown > scrollPositionToShow + 10) {
+            header.style.top='-10%';
+        } else {
+            header.style.top='0';
+        }
+        
+        if (scrollDown > this.document.documentElement.scrollTop) {
+            if (!scrollDirection) {
+                scrollDirection = true;
+                scrollPositionToShow = this.document.documentElement.scrollTop;
+            } else {
+                scrollDown = this.document.documentElement.scrollTop;
+            }
+        } else {
+            if (!scrollDirection) {
+                scrollDown = this.document.documentElement.scrollTop;
+            } else {
+                scrollDirection = false;
+                scrollPositionToShow = this.document.documentElement.scrollTop;
+            }
+        }
+    });
+});
