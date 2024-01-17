013042830e14a18b659674d15b3eda4e17c299cd
NickGhignatti <nick.ghignatti@gmail.com>
2024-01-17 16:04:35 +0100

    Settings update

--- backend/settings.php
+++ backend/settings.php
@@ -13,11 +13,19 @@ if (isset($_POST['profile-image'])) {
 } else {
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $new_password = $_POST['new_password'];
     $mail = $_POST['email'];
 
-    $result = '';
+    $description = $_POST['description'];
 
-    echo 'Here';
+    $user->update_description($description, $_SESSION['mail']);
+    $user->update_personal('username', $username, $_SESSION['user_id']);
+    $_SESSION['username'] = $username;
+    $user->update_personal('mail', $mail, $_SESSION['user_id']);
+    $_SESSION['mail'] = $mail;
+    if ($user->validate_password($_SESSION['mail'], $password)) {
+        $user->update_personal('password', password_hash($new_password, PASSWORD_DEFAULT), $_SESSION['user_id']);
+    }
 }
 
-// header('Location: ../frontend/pages/homepage_desktop.html');
+header('Location: ../frontend/pages/homepage_desktop.html');
