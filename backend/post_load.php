8b9f5fd730cd248ca76ac2cea2aafd6b211ce5ae
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2024-01-23 16:58:10 +0100

    homepage-feed almost done

--- backend/post_load.php
+++ backend/post_load.php
@@ -5,6 +5,10 @@ session_start();
 
 $p = new Post();
 
-$result = $p->get_all_following($_SESSION['user_id'], $_POST['limit']);
-
-echo json_encode($result);
+if (isset($_POST['func'])) {
+    $result = $p->get_post_image($_POST['id']);
+    echo $result;
+} else {
+    $result = $p->get_all_following($_SESSION['user_id'], $_POST['limit']);
+    echo json_encode($result);
+}
