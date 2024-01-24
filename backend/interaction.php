cd0b9d4347d9adfd7a2ddbe652601e65fb61b586
NickGhignatti <nick.ghignatti@gmail.com>
2024-01-24 14:57:59 +0100

    Add api for notifications

---
+++ backend/interaction.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'database.php';
+
+class Interaction {
+    function get_notifications($user_id) {
+        global $db;
+
+        $query = "SELECT Users.username, Interactions.interaction, Interactions.tp 
+            FROM Interactions 
+            INNER JOIN Posts ON Posts.id = Interactions.post_id 
+            INNER JOIN Users ON Users.id = Interactions.user_id 
+            WHERE Posts.user_id = ? ";
+
+        $statement = $db->prepare($query);
+        $statement->prepare('i', $user_id);
+        $statement->execute();
+        $result = $statement->get_result();
+
+        return $result->fetch_all();
+    }
+}
